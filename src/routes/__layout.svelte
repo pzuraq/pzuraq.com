deb54faf4467fe8638fdabe03afeef090d66e367
Chris Hewell Garrett <chris@bitski.com>
2022-04-07 09:28:36 -0400

    Add prefetch to art page

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -12,7 +12,7 @@
 <nav
   class="w-full p-4 md:p-8 md:pr-0 2xl:w-80 lg:w-64 md:w-36 md:fixed md:h-screen font-cursive text-center flex flex-col-reverse md:flex-col overflow-y-scroll"
 >
-  <a href="/art" class="-mt-8 -mb-4 mx-4 md:m-0 hover:scale-105">
+  <a sveltekit:prefetch href="/art" class="-mt-8 -mb-4 mx-4 md:m-0 hover:scale-105">
     <img
       src="/assets/store-day.png"
       alt="A peaceful little bookstore"
