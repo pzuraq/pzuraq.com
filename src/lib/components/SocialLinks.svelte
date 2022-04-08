87429a9b2d2e4254cdeb64547e12b39194d2bc25
Chris Hewell Garrett <chris@bitski.com>
2022-04-08 09:17:25 -0400

    add rss icon/link

--- src/lib/components/SocialLinks.svelte
+++ src/lib/components/SocialLinks.svelte
@@ -9,6 +9,9 @@
   <a href="https://github.com/pzuraq" aria-label="My Github profile" class="hover:text-primary">
     <i class="icon-github" />
   </a>
+  <a href="/rss" aria-label="My RSS feed" class="hover:text-primary">
+    <i class="icon-rss" />
+  </a>
   <a
     href="https://matrix.to/#/@pzura:matrix.org"
     aria-label="My Matrix profile"
