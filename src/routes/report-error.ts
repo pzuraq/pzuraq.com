2023ca8c2f62d15533fd58246afbf964ca7737d0
Chris Hewell Garrett <chris@bitski.com>
2022-04-27 08:09:47 -0400

    Add error reporting and minor updates to Four Eras

---
+++ src/routes/report-error.ts
@@ -0,0 +1,20 @@
+import type { RequestHandler } from '@sveltejs/kit';
+
+export const post: RequestHandler = async ({ request, locals }) => {
+  let { error } = await request.json();
+
+  if (error.message) {
+    error.message = `CLIENT ERROR: ${error.message}`;
+  } else if (typeof error === 'string') {
+    error = {
+      message: `CLIENT ERROR: ${error}`,
+    };
+  }
+
+  const err = new Error(error.message);
+  err.stack = error.stack ?? '';
+
+  locals.sentry?.captureException(err);
+
+  return new Response(JSON.stringify({ success: true }));
+};
